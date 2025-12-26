f030c92a20bf97f8ebfac3adb5c4aa07f1e73063
Kyra Meier <kyraprmeier@gmail.com>
2025-12-26 17:29:29 -0500

    edit

--- script.js
+++ script.js
@@ -3,12 +3,8 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYW5nbGUta3ByIiwiYSI6ImNtZjJybG10YzFkbnUycG80N
 
 // Initialize Mapbox map
 const map = new mapboxgl.Map({
-  container: 'map', // container ID
-  // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+  container: 'map', 
   style: 'mapbox://styles/angle-kpr/cmjnff242007j01sgah5yd1a5',
   center: [45.521728, -73.584887],
-  zoom: 5.8,
-  minZoom: 5.8,
-  maxZoom: 20,
-  maxBounds:[[-25, 62], [-10, 67]]
+  maxZoom: 19,
 }); 
